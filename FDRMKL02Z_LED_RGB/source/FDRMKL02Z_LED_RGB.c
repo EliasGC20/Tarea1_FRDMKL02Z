36b05961a860f21ac1907ab9892fe1cd3a1e156a
DESKTOP-GL45V2I\HP_142 <eliasgarcesdc@unimagdalena.edu.co>
2021-01-12 22:43:47 -0500

    Prueba de delay con 'P' y encendido de los 3 leds al tiempo.

--- FDRMKL02Z_LED_RGB/source/FDRMKL02Z_LED_RGB.c
+++ FDRMKL02Z_LED_RGB/source/FDRMKL02Z_LED_RGB.c
@@ -1,37 +1,8 @@
-/*
- * Copyright 2016-2021 NXP
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+// Realizar un proyecto que utilice los leds RGB del microcontrolador FRDM-KL02Z realizado de la siguiente manera:
+// V ON verde - v OFF verde
+// R ON rojo - r OFF rojo
+// A ON azul - a OFF azul
  
-/**
- * @file    FDRMKL02Z_LED_RGB.c
- * @brief   Application entry point.
- */
 #include <stdio.h>
 #include "board.h"
 #include "peripherals.h"
@@ -39,27 +10,14 @@
 #include "clock_config.h"
 #include "MKL02Z4.h"
 #include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
-
-/* TODO: insert other definitions and declarations here. */
 
 
-//MACROS
+//Declaracion de las MACROS
 
 status_t uart0Inicializar(uint32_t);
 uint8_t uart0NuevosDatosEnBuffer(void);
 status_t uart0LeerByteDesdeBufferCircular(uint8_t *);
-/*
- * @brief   Application entry point.
- */
-//void delay(void)
-//{
-//    volatile uint32_t i = 0;
-//    for (i = 0; i < 0xFFFFFFFF; ++i)
-//    {
-//        __asm("NOP"); /* delay */
-//    }
-//}
+
 
 int main(void) {
 
